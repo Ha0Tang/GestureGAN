7556fbcf360bed96da2e52c1c80e55fbc941b7ab
Ha0Tang <haotang@pku.edu.cn>
2019-07-19 23:45:09 +0200

    fix typo

--- datasets/download_gesturegan_dataset.sh
+++ datasets/download_gesturegan_dataset.sh
@@ -8,9 +8,9 @@ fi
 echo "Specified [$FILE]"
 
 URL=http://disi.unitn.it/~hao.tang/dataset/GestureGAN/$FILE.tar.gz
-TAR_FILE=./datasets/$FILE.tar.gz
-TARGET_DIR=./datasets/$FILE/
+TAR_FILE=./$FILE.tar.gz
+TARGET_DIR=./$FILE/
 wget -N $URL -O $TAR_FILE
 mkdir -p $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./datasets/
+tar -zxvf $TAR_FILE -C ./
 rm $TAR_FILE

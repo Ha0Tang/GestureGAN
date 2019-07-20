2e180e454cf1ac35d57acbeb0096755da49401ea
Ha0Tang <haotang@pku.edu.cn>
2019-07-21 00:10:35 +0200

    update datasets download link

--- datasets/download_gesturegan_dataset.sh
+++ datasets/download_gesturegan_dataset.sh
@@ -1,13 +1,14 @@
 FILE=$1
 
-if [[ $FILE != "ntu_image_skeleton" && $FILE != "senz3d_skeleton" ]]; then
-  echo "Available datasets are ntu_image_skeleton, senz3d_skeleton"
-  exit 1
+if [[ $FILE != "ntu_image_skeleton" && $FILE != "senz3d_skeleton" ]]; 
+	then echo "Available datasets are ntu_image_skeleton, senz3d_skeleton"
+	exit 1
 fi
+
 
 echo "Specified [$FILE]"
 
-URL=http://disi.unitn.it/~hao.tang/datasets/GestureGAN/$FILE.tar.gz
+URL=http://disi.unitn.it/~hao.tang/uploads/datasets/GestureGAN/$FILE.tar.gz
 TAR_FILE=./datasets/$FILE.tar.gz
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $TAR_FILE

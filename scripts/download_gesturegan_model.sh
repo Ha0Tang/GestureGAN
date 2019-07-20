87882a2bb3d94d4e22817de3bdf55b6196d5dbd0
Ha0Tang <haotang@pku.edu.cn>
2019-07-20 12:30:50 +0200

    add pretrained models download links

---
+++ scripts/download_gesturegan_model.sh
@@ -0,0 +1,14 @@
+FILE=$1
+
+echo "Note: available models are dayton_a2g_256, dayton_g2a_256, cvusa, dayton_a2g_64, dayton_g2a_64, ntu and senz3d"
+echo "Specified [$FILE]"
+
+URL=http://disi.unitn.it/~hao.tang/uploads/models/GestureGAN/${FILE}_pretrained.tar.gz
+TAR_FILE=./checkpoints/${FILE}_pretrained.tar.gz
+TARGET_DIR=./checkpoints/${FILE}_pretrained/
+
+wget -N $URL -O $TAR_FILE
+
+mkdir -p $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./checkpoints/
+rm $TAR_FILE
